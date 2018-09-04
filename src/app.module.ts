264a17656c369782875c68856eb479485c8615a1
Rodrigo Pavez <rpavez@gmail.com>
2018-09-04 13:05:05 -0300

    postgres & jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { DatabaseModule } from 'database/database.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  // imports: [
+  //   GraphQLModule.forRoot({
+  //     typePaths: ['./**/*.graphql'],
+  //   }),
+  // ],
+  imports: [ AuthModule, DatabaseModule ],
   controllers: [AppController],
   providers: [AppService],
 })
